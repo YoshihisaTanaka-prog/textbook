058971c86f7c463df741a88b0642af868c8a8869
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2023-03-06 12:51:19 +0900

    apiの修正

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,8 +2,8 @@ class Subject < ApplicationRecord
     def hash_format
         selection_id = - self.id
         main = Main.find_by(selection_id: selection_id)
-        main_hash = {}
-        main_hash = main.hash_format if main
-        return {subject: self.name, main: main_hash}
+        main_id = 0
+        main_id = main.id if main
+        return {subject: self.name, main_id: main_id}
     end
 end
