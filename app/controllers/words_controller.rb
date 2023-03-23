8d337a7f6684dc4bc48dec76783f4dacf075ffb0
YoshihisaTanaka-prog <72920523+YoshihisaTanaka-prog@users.noreply.github.com>
2023-03-23 12:41:45 +0900

    単語の新規追加ページにこれまでの単語も表示するよう変更

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -21,6 +21,7 @@ class WordsController < ApplicationController
   def new
     @word = Word.new
     @student = Student.find(session[:student_id])
+    @current_time_words = Word.where(student_id: @student.id, time: @student.current_time)
   end
 
   # GET /words/1/edit
