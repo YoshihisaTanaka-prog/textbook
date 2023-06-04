8a8fa990ecfe91cd1f70486a1f09aaa55cad2ebe
TanakaYoshihisa <1sc16346k@gmail.com>
2023-06-04 20:48:02 +0900

    解決策から選択肢に戻りやすくした。

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -2,4 +2,13 @@ class Solution < ApplicationRecord
     def hash_format
         return {id: self.id, kana: self.kana, title: self.title, content: self.content}
     end
+
+    def selections
+        ret = []
+        SolutionSelection.where(solution_id: self.id).each do |ss|
+            s = Selection.find_by(id: ss.selection_id)
+            ret << s if s
+        end
+        return ret
+    end
 end
